7b8ab900bfe43400d237cca4a02d24515016b8d6
Yash-Vashishth <imyashvashishth05@gmail.com>
2025-04-20 20:24:25 +0530

    y

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,10 +1,42 @@
+"use client"
 import React from 'react'
+import { Sheet, SheetContent, SheetTrigger } from "../components/ui/sheet";
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import { CiMenuFries } from 'react-icons/ci';
+
+const links = [
+    {
+        name: "home",
+        path: "/",
+    },
+    {
+        name: "services",
+        path: "/services",
+    },
+    {
+        name: "resume",
+        path: "/resume",
+    },
+    {
+        name: "work",
+        path: "/work",
+    },
+    {
+        name: "contact",
+        path: "/contact",
+    },
+]
 
 const MobileNav = () => {
+    const pathname = usePathname();
     return (
-        <div>
-            hcxa
-        </div>
+        <Sheet>
+            <SheetTrigger className='flex justify-center items-center'>
+                <CiMenuFries className='text-[32px] text-accent' />
+            </SheetTrigger>
+            <SheetContent>content</SheetContent>
+        </Sheet>
     )
 }
 
