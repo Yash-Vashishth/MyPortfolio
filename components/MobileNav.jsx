b7aa078d8ad4ea3180f14f93ef8756b03a44fa1f
Yash-Vashishth <imyashvashishth05@gmail.com>
2025-04-20 21:31:34 +0530

    y

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -35,7 +35,24 @@ const MobileNav = () => {
             <SheetTrigger className='flex justify-center items-center'>
                 <CiMenuFries className='text-[32px] text-accent' />
             </SheetTrigger>
-            <SheetContent>content</SheetContent>
+
+            <SheetContent className='flex flex-col'>
+                <div className='mt-32 mb-40 text-center text-2xl'>
+                    <Link href='/'>
+                        <h1 className='text-4xl font-semibold'>Yash<span className='text-accent'>.</span></h1>
+                    </Link>
+                </div>
+
+                <nav className='flex flex-col justify-center items-center gap-6'>
+                    {links.map((link, index) => {
+                        return (
+                            <Link href={link.path} key={index}>
+                                {link.name}
+                            </Link>
+                        )
+                    })}
+                </nav>
+            </SheetContent>
         </Sheet>
     )
 }
