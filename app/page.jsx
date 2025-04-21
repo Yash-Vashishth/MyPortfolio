aef2e3a353a5e4bb38362f47452c32f139732b7f
Yash-Vashishth <imyashvashishth05@gmail.com>
2025-04-21 22:07:01 +0530

    y

--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ const Home = () => {
         <div className='flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24'>
 
           {/* text */}
-          <div className='text-center xl:text-left'>
+          <div className='text-center xl:text-left order-2 xl:order-none'>
             <span className='text-xl'>Software Developer</span>
             <h1 className='h1'>
               Hello I am <br /> <span className='text-accent '>Yash Vashishth</span>
@@ -37,7 +37,7 @@ const Home = () => {
           </div>
 
           {/*photo*/}
-          <div>
+          <div className='order-1 xl:order-none mb-8 xl:mb-0'>
             <Photo />
           </div>
 
--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,9 +1,57 @@
+"use client";
+import { motion } from 'motion/react';
+import Image from 'next/image';
 import React from 'react'
 
 const Photo = () => {
     return (
-        <div>
-            Photo
+        <div className='w-full h-full relative'>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{
+                    opacity: 1,
+                    transition: { delay: 1, duration: 0.4, ease: "easeIn" },
+                }}
+            >
+                {/*image*/}
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{
+                        opacity: 1,
+                        transition: { delay: 2, duration: 0.4, ease: "easeInOut" },
+                    }}
+                    className='w-[300px] h-[300px] xl:w-[460px] xl:h-[460px] mix-blend-ligten absolute'
+                >
+                    <Image src="/assets/photo.png" priority quality={100} fill alt="" className='object-contain' />
+                </motion.div>
+
+                {/*circle*/}
+                <motion.svg className='w-[300px] xl:w-[460px] h-[300px] xl:h-[460px]'
+                    fill="transparent"
+                    viewBox="0 0 506 506"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <motion.circle
+                        cx="253"
+                        cy="253"
+                        r="250"
+                        stroke="#00ff99"
+                        strokeWidth="4"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        initial={{ strokeDasharray: "24 10 0 0" }}
+                        animate={{
+                            strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+                            rotate: [120, 360],
+                        }}
+                        transition={{
+                            duration: 20,
+                            repeat: Infinity,
+                            repeatType: "reverse",
+                        }}
+                    />
+                </motion.svg>
+            </motion.div>
         </div>
     )
 }
